010e68f317adcc9ddbd8612a24210e7cc3c584b4
SungJin Cho <34265718+csjin7@users.noreply.github.com>
2017-12-26 01:08:46 +0900

    Remote GitHub changed

--- Part02/src/chap15/PushPrint.java
+++ Part02/src/chap15/PushPrint.java
@@ -4,5 +4,7 @@ public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("local to Remote GitHub push file");
 		
+		System.out.println(" Remote GitHub changed");
+		
 	}
 }
