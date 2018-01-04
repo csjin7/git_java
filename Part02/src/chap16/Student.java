3543c843a4376f291ac5acc89c431c37584ec33d
csjin7 <csjin72@gmail.com>
2018-01-04 21:36:12 +0900

    update changes

--- Part02/src/chap16/Student.java
+++ Part02/src/chap16/Student.java
@@ -1,23 +1,50 @@
 package chap16;
 
-public class Student implements Comparable<Student> {
+public class Student  {
+	public enum Sex { MALE, FEMALE }
+	public enum City { Seoul, Pusan }
+	
 	private String name;
 	private int score;
-	public Student(String name, int score) {
+	private Sex sex;
+	private City city;
+	
+	
+	public Student(String name, int score, Sex sex) {
 		this.name = name;
 		this.score = score;
+		this.sex = sex;
 	}
+
+
+	public Student(String name, int score, Sex sex, City city) {
+		this.name = name;
+		this.score = score;
+		this.sex = sex;
+		this.city = city;
+	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public int getScore() {
 		return score;
 	}
-	
-	@Override
-	public int compareTo(Student o) {
-		return Integer.compare(score,  o.score);
+
+
+	public Sex getSex() {
+		return sex;
 	}
+
+
+	public City getCity() {
+		return city;
+	}
+	
+	
 	
 	
 }
